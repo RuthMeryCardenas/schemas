de1191e9cb9effc601071992842a43daa7c64f84
Moisés Cachay <xpktro@gmail.com>
2019-09-18 21:27:54 -0500

    feat(user-activity-feed): add user activity feed schemas tests

---
+++ src/__tests__/UserActivityFeedEventSchemas/index.spec.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose/browser');
+const { UserActivityFeedEventSchemas: { UserActivityFeedEventSchema } } = require('../../../')(mongoose);
+
+
+describe('UserActivityFeedEventSchema', () => {
+  it('should fail validation when missing fields are provided', () => {
+    const doc = new mongoose.Document({}, UserActivityFeedEventSchema);
+    expect(doc.validateSync().errors).toMatchSnapshot();
+  });
+
+  it('should successfully validate with proper values', (done) => {
+    const doc = new mongoose.Document({
+      doc: mongoose.Types.ObjectId(),
+      type: 'GenericEvent',
+    }, UserActivityFeedEventSchema);
+
+    return doc.validate((err) => {
+      expect(err).toBe(null);
+      done();
+    });
+  });
+});
